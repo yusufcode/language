30c94149da9fef8184f9abf6aeb7fefb5f0ab516
Yusuf Code <yusuf1code@gmail.com>
2022-05-17 19:44:51 +0300

    .env file added to config of dotenv

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const path = require('path')
 
 //APP USE
-dotenv.config()
+dotenv.config({path: '.env'})
 app.use(express.json())
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
